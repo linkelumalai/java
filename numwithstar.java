1b488d8a78b64c52267d3c2c915ece714a80cfff
lucky0413 <linkelumalai@gmail.com>
2021-09-21 13:23:55 +0530

    number with star

---
+++ numwithstar.java
@@ -0,0 +1,13 @@
+class numwithstar{
+	public static void main(String args[]){
+	int a=1;
+	for(int i=0; i<5; i++)
+	{
+	if(i%2==0)
+	System.out.print(a++);
+	else
+	System.out.print("*");
+ 
+	}
+}
+}
