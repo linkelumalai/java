5cb9d696231953ba0daf5ccf53423c7510246c4d
lucky0413 <linkelumalai@gmail.com>
2021-09-21 13:24:52 +0530

    12321

---
+++ incdec.java
@@ -0,0 +1,13 @@
+class incdec{
+	public static void main(String args[]){
+	int number=5; 
+	int x=1; 
+	for(int i =0; i<number; i++)
+{
+	if(i<number/2)
+	System.out.print(x++); 
+	else
+	System.out.print(x--);
+}
+}
+}
