f219c3808f0bee35e5f20cd3ce0e4f9666cd26b4
lucky0413 <linkelumalai@gmail.com>
2021-10-02 10:46:07 +0530

    square numbers

---
+++ squarenumbers.java
@@ -0,0 +1,17 @@
+class squarenumbers{
+	public static void main(String args[])
+		
+{
+		int n=5; 
+		char ch='a';
+		for (int i=0; i<n; i++){
+			for (int j=0; j<n; j++)
+{
+			System.out.print( ch++ +" ");
+}
+			System.out.println();
+			ch='a';
+}
+}
+}
+
