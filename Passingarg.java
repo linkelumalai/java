3cedcc9bea0d5a5b724ad28cbc408e9dec1d6d67
lucky0413 <linkelumalai@gmail.com>
2021-10-02 10:36:41 +0530

    passing argument

---
+++ Passingarg.java
@@ -0,0 +1,13 @@
+class Passingarg{
+    static void test(int a)
+    {
+        System.out.println("value is : " +a);
+    }
+    public static void main(String[] args){
+
+        test(34);
+        test(5);
+        test(78979);
+
+    }
+}
