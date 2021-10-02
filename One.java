37fad04a5891262a4fc18207626e382d552658c8
lucky0413 <linkelumalai@gmail.com>
2021-10-02 10:35:49 +0530

    simple input get

---
+++ One.java
@@ -0,0 +1,9 @@
+import java.util.Scanner; 
+class One{
+    public static void main(String[] args){
+        Scanner var=new Scanner(System.in);
+        System.out.println("Enter the value");
+        int num=var.nextInt(); 
+        System.out.println("value is :" +num);
+    }
+}
