7367b5419a78c47e89c8b9cd05253aa8762acd2f
lucky0413 <linkelumalai@gmail.com>
2021-10-02 10:39:26 +0530

    using return

---
+++ returnvalarg.java
@@ -0,0 +1,9 @@
+class returnvalarg{
+    static int addtion(int num ,int num1){
+        return num+num1; 
+    }
+    public static void main(String[] args){
+        int sum=addtion(10,20);
+        System.out.println(sum);
+    } 
+}
