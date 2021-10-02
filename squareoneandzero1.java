4a483898826fd505f23d11f922a1a860262ba1f8
lucky0413 <linkelumalai@gmail.com>
2021-10-02 10:47:25 +0530

    square 1 0

---
+++ squareoneandzero1.java
@@ -0,0 +1,20 @@
+class squareoneandzero1{
+	public static void main(String args[])
+		
+{
+		int n=5; 
+		//int a=1;
+		for (int i=0; i<n; i++){
+			for (int j=0; j<n; j++)
+{
+			if(i%2==0)
+			System.out.print( "1" +" ");
+			else 
+			System.out.print("0" +" ");
+}
+			System.out.println();
+		//	ch='a';
+}
+}
+}
+
