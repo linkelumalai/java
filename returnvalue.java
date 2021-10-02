bf6b0fad8e40860050629bd9bfe8b7861e8583d4
lucky0413 <linkelumalai@gmail.com>
2021-10-02 10:39:58 +0530

    using return

---
+++ returnvalue.java
@@ -0,0 +1,14 @@
+class returnvalue{
+    static int dip(){
+        return 25; 
+    }
+    static String dip1(){
+        return " lucky";
+    }
+    public static void main(String[] args){
+        int val=dip(); 
+        System.out.println(val);
+        String name=dip1();
+        System.out.println(name);
+    }
+}
