dd28cbc91853f8abb6db3b39ec1665e6eb7b1c18
lucky0413 <linkelumalai@gmail.com>
2021-10-02 10:31:25 +0530

    multiple arguments in method

---
+++ multiarg.java
@@ -0,0 +1,25 @@
+class multiarg{
+    static void addtion(int num,int num1)
+    {
+        int a=num+num1;
+        System.out.println( "addtion value is : " +a);
+       // cube(8);
+    }
+    static void sub(int num,int num1)
+    {
+        int a=num-num1; 
+        System.out.println(" subration of the value is : " +a);
+    }
+    static void mul(int num,int num1)
+    {
+        int a=num*num1; 
+        System.out.println(" multiplicationj of the value is : " +a);
+    }
+    public static void main(String[] args){
+
+        addtion(2234332,43422);
+        sub(4242,4343);
+        mul(7979849,43453);
+
+    }
+}
