c69fab0e58b19f73475529c579105a590ee43c8b
lucky0413 <linkelumalai@gmail.com>
2021-10-02 10:45:37 +0530

    square middle

---
+++ squaremid.java
@@ -0,0 +1,20 @@
+class squaremid{
+	public static void main(String args[])
+		
+{
+		int n=5; 
+		//int a=1;
+		for (int i=0; i<n; i++){
+			for (int j=0; j<n; j++)
+{
+			if(i==n/2||j==n/2)
+			System.out.print( "*" +" ");
+			else 
+			System.out.print(" " +" ");
+}
+			System.out.println();
+		//	ch='a';
+}
+}
+}
+
