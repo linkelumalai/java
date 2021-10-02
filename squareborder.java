2f5d9f5a91ce81dea5cdc9e7107f3b946ff96b81
lucky0413 <linkelumalai@gmail.com>
2021-10-02 10:43:35 +0530

    square border

---
+++ squareborder.java
@@ -0,0 +1,20 @@
+class squareborder{
+	public static void main(String args[])
+		
+{
+		int n=5; 
+		//int a=1;
+		for (int i=0; i<n; i++){
+			for (int j=0; j<n; j++)
+{
+			if(i==0||j==0||i==n-1||j==n-1)j
+			System.out.print( "*" +" ");
+			else 
+			System.out.print(" " +" ");
+}
+			System.out.println();
+		//	ch='a';
+}
+}
+}
+
