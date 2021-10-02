5138b7380fd3888a5ae688e2b1daccd6ef99598d
lucky0413 <linkelumalai@gmail.com>
2021-10-02 10:37:13 +0530

    perfect divisors

---
+++ PerfectDivisor.java
@@ -0,0 +1,19 @@
+class PerfectDivisor{
+    public static void main(String[] args)
+    {
+        long n=60; 
+        int i=0; 
+        //long a=1;
+       // while(a<=n)
+     for(i=1;i<n;i++)
+        {
+            if(n%i==0)
+            System.out.println(i);
+            
+           
+
+        }
+        
+    }
+}
+
