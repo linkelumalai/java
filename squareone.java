141dae17df58661a10194fc7eff333b7104888c6
lucky0413 <linkelumalai@gmail.com>
2021-10-02 10:47:57 +0530

    square

---
+++ squareone.java
@@ -0,0 +1,17 @@
+class squareone{
+	public static void main(String args[])
+		
+{
+		int a=1; 
+		
+	for (int i=0; i<5; i++){
+		for (int j=0; j<5; j++)
+{
+			System.out.print( a +" ");
+}
+			System.out.println();
+			a++;
+}
+}
+}
+
